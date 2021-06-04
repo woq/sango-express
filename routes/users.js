76f670d47b144b1d1eddf7372f0d6a5fc91d9658
woq <33190773+woq@users.noreply.github.com>
2021-06-05 00:14:18 +0800

    api for sango
    1.cors跨域处理 完成
    2.添加axios 完成
    3.添加json模板
    4.检测url符合标准
    5.添加到json文件尾部

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
+const axios = require("axios");
 var router = express.Router();
 
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+  checkLinks(req.body.urls);
+  res.send(req.body);
 });
 
+//检查链接是否符合标准,检测是否访问正常,访问正常则添加在json尾部
+function checkLinks(links) {
+  //首先利用/r/n /n来分割成为数组
+  links = links.split(/[\s\n]/)
+  //检测是否为URL格式
+
+
+  //添加到json文件尾部
+  for (let key in links) {
+    console.log(links[key])
+    //如果是,尝试访问,获得返回HTTP状态码,并从里面获得制定数据
+    axios
+        .get(links[key])
+        .then(r => console.log(r.status) )
+        .catch(err => console.log(err.response.status))
+  }
+}
 module.exports = router;
